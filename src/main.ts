5656f34d5f2a082ef0cf50fbc0a2fb06ea6b0ba2
minhvuong2k3 <vominhvuong15112003@gmail.com>
2024-03-06 13:44:17 +0700

    swagger vs firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,60 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+console.log(process.env.PORT);
+console.log(process.env.DB_UNAME);
+console.log(process.env.DB_PASSWORD);
+import { applicationDefault } from 'firebase-admin/app';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  //PIPELINES
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //Remove all unknown properties when validate DTO
+      whitelist: true,
+    }),
+  );
+
+  //FIREBASE ADMIN
+  // Initialize the firebase admin app
+  admin.initializeApp({
+    credential: applicationDefault(),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+
+  //SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('LinkID Server')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
