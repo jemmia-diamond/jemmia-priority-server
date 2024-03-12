9805730509ad6792b71a5b055c5addcf2e7e87ff
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2024-03-12 16:14:16 +0700

    U Permision

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -7,16 +7,22 @@ import {
   Delete,
   Query,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CouponDto, CouponSearchDto } from './dto/coupon.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { EUserRole } from '../user/enums/user-role.enum';
 
 @ApiTags('Coupon')
 @Controller('coupon')
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(EUserRole.admin)
   @Post()
   create(@Body() data: CouponDto) {
     return this.couponService.createCoupon(data);
@@ -32,11 +38,15 @@ export class CouponController {
     return this.couponService.findCoupon(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(EUserRole.admin)
   @Put(':id/status/enable')
   enableStatus(@Param('id') id: string) {
     return this.couponService.toggleStatus(+id, true);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(EUserRole.admin)
   @Put(':id/status/disable')
   disableStatus(@Param('id') id: string) {
     return this.couponService.toggleStatus(+id, false);
@@ -47,6 +57,8 @@ export class CouponController {
   //   return this.couponService.updateCoupon(+id, data);
   // }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(EUserRole.admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.couponService.deleteCoupon(+id);
