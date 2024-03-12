ee284f5d7cf58d937ea60d2928a9408b5f4b4993
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2024-03-12 16:12:03 +0700

    U Coupon Service

---
+++ src/coupon/dto/coupon.dto.ts
@@ -0,0 +1,5 @@
+import { HaravanCouponDto } from '../../haravan/dto/haravan-coupon.dto';
+
+export class CouponDto extends HaravanCouponDto {}
+
+export class CouponSearchDto extends HaravanCouponDto {}
