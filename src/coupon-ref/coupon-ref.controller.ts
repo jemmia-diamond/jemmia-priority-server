7c944947fdf73bcdfdb6999b32d74de5dabda916
Kretos Labs <nhatsdevil@gmail.com>
2024-03-27 19:29:57 +0700

    U Coupon Query

--- src/coupon-ref/coupon-ref.controller.ts
+++ src/coupon-ref/coupon-ref.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { CouponRefService } from './coupon-ref.service';
 import { CreateCouponRefDto } from './dto/create-coupon-ref.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { EUserRole } from '../user/enums/user-role.enum';
@@ -19,6 +19,7 @@ import { CouponRef } from './entities/coupon-ref.entity';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { InviteCouponRefDto } from './dto/invite-coupon-ref.dto';
 import { RequestPayload } from '../types/controller.type';
+import { ECouponRefType } from './enums/coupon-ref.enum';
 
 @ApiTags('Coupon Referral')
 @ApiBearerAuth()
@@ -51,29 +52,28 @@ export class CouponRefController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('/invite/partner')
-  async findAllInvitePartner(
-    @Request() req: RequestPayload,
-    @Query('page') page = 1,
-    @Query('limit') limit = 10,
-  ): Promise<Pagination<CouponRef>> {
-    limit = Math.min(50, limit); // Giới hạn limit tối đa là 50
-    return this.couponRefService.findAllInvitePartner(
-      req.user.id,
-      req.user.role,
-      page,
-      limit,
-    );
-  }
-
-  @UseGuards(JwtAuthGuard)
   @Get('/invite/:userId')
-  async findInvite(
+  @ApiQuery({
+    name: 'used',
+    type: Number,
+    description: '',
+    required: false,
+  })
+  async findAllInvite(
     @Request() req: RequestPayload,
     @Param('userId') userId: string,
-  ) {
-    return this.couponRefService.findInvite(
-      req.user.role != EUserRole.admin ? req.user.id : userId,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('type') type: ECouponRefType,
+    @Query('used') used: string | null,
+  ): Promise<Pagination<CouponRef>> {
+    limit = Math.min(50, limit); // Giới hạn limit tối đa là 50
+    return this.couponRefService.findAllInvite(
+      req.user.role === EUserRole.admin ? userId : req.user.id,
+      type,
+      used ? Boolean(Number(used)) : null,
+      page,
+      limit,
     );
   }
 
