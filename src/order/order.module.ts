3f97fa226497cc3f7c2a7e4924758f5fa8832ce2
huy17212 <hahuytri2K3@gmail.com>
2024-03-28 10:21:49 +0700

    modify

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,16 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { HaravanModule } from '../haravan/haravan.module';
 import { User } from '../user/entities/user.entity';
 import { CouponRefModule } from '../coupon-ref/coupon-ref.module';
-import { UserService } from '../user/user.service';
 import { Order } from './entities/order.entity';
+import { CouponRefService } from '../coupon-ref/coupon-ref.service';
+import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Order]),
     HaravanModule,
     CouponRefModule,
+    UserModule,
   ],
   controllers: [OrderController],
-  providers: [OrderService, UserService],
+  providers: [OrderService, CouponRefService, UserService],
 })
 export class OrderModule {}
