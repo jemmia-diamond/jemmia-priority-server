963c21c64ba35263accfa54a21beb48f8eb2c6d1
minhvuong2k3 <92205328+minhvuong2k3@users.noreply.github.com>
2024-04-01 21:41:16 +0700

    api get rank info

--- src/customer-rank/customer-rank.module.ts
+++ src/customer-rank/customer-rank.module.ts
@@ -4,9 +4,11 @@ import { CustomerRankService } from './customer-rank.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { Order } from '../order/entities/order.entity';
+import { CustomerRankController } from './customer-rank.controller';
 
 @Module({
   imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([User, Order])],
+  controllers: [CustomerRankController],
   providers: [CustomerRankService],
 })
 export class CustomerRankModule {}
