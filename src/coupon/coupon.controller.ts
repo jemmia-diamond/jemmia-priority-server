28c402075f3b2aa3db61efa72182c8e5d41dfa3e
Nhats <nhatsdevil@gmail.com>
2024-04-14 22:21:20 +0700

    U

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -1,25 +1,17 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Param,
-  Delete,
   Query,
-  Put,
   UseGuards,
   Request,
 } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { CouponDto, CouponSearchDto } from './dto/coupon.dto';
+import { CouponDto } from './dto/coupon.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { EUserRole } from '../user/enums/user-role.enum';
-import { GiftDto } from './dto/gift.dto';
-import { Coupon } from './entities/coupon.entity';
-import { Pagination } from 'nestjs-typeorm-paginate';
-
 @ApiTags('Coupon')
 @ApiBearerAuth()
 @Controller('coupon')
@@ -33,12 +25,12 @@ export class CouponController {
     return this.couponService.createCoupon(data);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(EUserRole.admin)
-  @Post('/gift')
-  createGift(@Body() data: GiftDto) {
-    return this.couponService.createGift(data);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(EUserRole.admin)
+  // @Post('/gift')
+  // createGift(@Body() data: GiftDto) {
+  //   return this.couponService.createGift(data);
+  // }
 
   @UseGuards(JwtAuthGuard)
   @Post('/discount-receive')
@@ -46,79 +38,79 @@ export class CouponController {
     return this.couponService.discountReceive(req.user.id, money);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Post('/gift/:id')
-  receiveGift(@Request() req, @Param('id') id: string) {
-    return this.couponService.receiveGift(req.user.id, id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Post('/gift/:id')
+  // receiveGift(@Request() req, @Param('id') id: string) {
+  //   return this.couponService.receiveGift(req.user.id, id);
+  // }
 
-  @Get()
-  findAll(@Query() query: CouponSearchDto) {
-    return this.couponService.findAllCoupon(query);
-  }
+  // @Get()
+  // findAll(@Query() query: CouponSearchDto) {
+  //   return this.couponService.findAllCoupon(query);
+  // }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('/gift')
-  async findAllGift(
-    @Request() req,
-    @Query('page') page = 1,
-    @Query('limit') limit = 10,
-  ): Promise<Pagination<Coupon>> {
-    limit = Math.min(50, limit); // Giới hạn limit tối đa là 50
-    return this.couponService.findAllGift(
-      req.user.id,
-      req.user.role,
-      page,
-      limit,
-    );
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Get('/gift')
+  // async findAllGift(
+  //   @Request() req,
+  //   @Query('page') page = 1,
+  //   @Query('limit') limit = 10,
+  // ): Promise<Pagination<Coupon>> {
+  //   limit = Math.min(50, limit); // Giới hạn limit tối đa là 50
+  //   return this.couponService.findAllGift(
+  //     req.user.id,
+  //     req.user.role,
+  //     page,
+  //     limit,
+  //   );
+  // }
 
-  @Get('/gift/:id')
-  async findOneGift(@Request() req, @Param('id') id: string) {
-    return this.couponService.findGiftById(req.user.id, req.user.role, id);
-  }
+  // @Get('/gift/:id')
+  // async findOneGift(@Request() req, @Param('id') id: string) {
+  //   return this.couponService.findGiftById(req.user.id, req.user.role, id);
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.couponService.findCoupon(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.couponService.findCoupon(+id);
+  // }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(EUserRole.admin)
-  @Put(':id/status/enable')
-  updateGift(@Param('id') id: string, @Body() data: GiftDto) {
-    return this.couponService.updateGift(id, data);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(EUserRole.admin)
+  // @Put(':id/status/enable')
+  // updateGift(@Param('id') id: string, @Body() data: GiftDto) {
+  //   return this.couponService.updateGift(id, data);
+  // }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(EUserRole.admin)
-  @Put(':id/gift')
-  enableStatus(@Param('id') id: string) {
-    return this.couponService.toggleStatus(+id, true);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(EUserRole.admin)
+  // @Put(':id/gift')
+  // enableStatus(@Param('id') id: string) {
+  //   return this.couponService.toggleStatus(+id, true);
+  // }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(EUserRole.admin)
-  @Put(':id/status/disable')
-  disableStatus(@Param('id') id: string) {
-    return this.couponService.toggleStatus(+id, false);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(EUserRole.admin)
+  // @Put(':id/status/disable')
+  // disableStatus(@Param('id') id: string) {
+  //   return this.couponService.toggleStatus(+id, false);
+  // }
 
   // @Put(':id')
   // update(@Param('id') id: string, @Body() data: CouponDto) {
   //   return this.couponService.updateCoupon(+id, data);
   // }
-  @UseGuards(JwtAuthGuard)
-  @Roles(EUserRole.admin)
-  @Delete(':id/gift')
-  removeGift(@Param('id') id: string) {
-    return this.couponService.deleteGift(id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(EUserRole.admin)
+  // @Delete(':id/gift')
+  // removeGift(@Param('id') id: string) {
+  //   return this.couponService.deleteGift(id);
+  // }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(EUserRole.admin)
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.couponService.deleteCoupon(+id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(EUserRole.admin)
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.couponService.deleteCoupon(+id);
+  // }
 }
