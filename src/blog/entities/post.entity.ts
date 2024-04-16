98244343a2d667bd40eab37baf9b0460aced4de8
Kretos Labs <nhatsdevil@gmail.com>
2024-04-16 10:21:33 +0700

    U User Role Arr

--- src/blog/entities/post.entity.ts
+++ src/blog/entities/post.entity.ts
@@ -12,8 +12,8 @@ export class Post {
   @Column('varchar', { nullable: true })
   pdfUrl: string;
 
-  @Column('enum', { enum: EUserRole, nullable: true })
-  userRole: EUserRole;
+  @Column('jsonb', { nullable: true })
+  userRole: EUserRole[];
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdDate: Date;
