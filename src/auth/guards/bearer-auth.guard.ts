b3e18f196174135e38e2618cabc96f38e8dc7bdd
Hoàng Hưng <hoanghung@MacBook-Pro-cua-Hoang.local>
2025-07-30 15:28:50 +0700

    fix bearer

--- src/auth/guards/bearer-auth.guard.ts
+++ src/auth/guards/bearer-auth.guard.ts
@@ -11,22 +11,20 @@ export class BearerAuthGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     if (!this.validToken) {
-      throw new UnauthorizedException('Bearer token not configured');
+      return false;
     }
 
     const req = context.switchToHttp().getRequest();
     const authHeader = req.headers['authorization'];
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      throw new UnauthorizedException(
-        'Missing or malformed Authorization header',
-      );
+      return false;
     }
 
     const token = authHeader.split(' ')[1];
 
     if (token !== this.validToken) {
-      throw new UnauthorizedException('Invalid token');
+      return false;
     }
 
     return true;
