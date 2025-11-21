84bb0faec80df82a8aa4763805b943f7e14f3eb1
BinDK <sarakenny123@yahoo.com>
2025-11-21 12:12:01 +0700

    feat(priority): adding customer name, rank, role

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -450,6 +450,9 @@ export class UserService {
         'trueCumulativeRevenue',
       )
       .addSelect(`c.totalPoint`, 'totalPoint')
+      .addSelect('c.name', 'name')
+      .addSelect('c.rank', 'rank')
+      .addSelect('c.role', 'role')
       .innerJoin('coupon_refs', 'cf', 'c.id = cf.ownerId')
       .innerJoin('orders', 'o', 'o.couponRefId = cf.id')
       .leftJoin('withdraws', 'wd', 'wd.userId = c.id')
@@ -477,6 +480,9 @@ export class UserService {
       pendingCashback: Number(result?.pendingCashback) || 0,
       pointAvailable: Number(result?.pointAvailable) || 0,
       totalPoint: Number(result?.totalPoint) || 0,
+      name: result?.name || '',
+      rank: Number(result?.rank) || 0,
+      role: result?.role || '',
     };
   }
 
