29ffb04aa73d8167d69d75dea6583b94116a66f6
BinDK <sarakenny123@yahoo.com>
2025-11-21 09:24:51 +0700

    feat(priority): Adding query field for coupon-ref endpoint

--- src/syncCrm/sync-crm.module.ts
+++ src/syncCrm/sync-crm.module.ts
@@ -3,9 +3,10 @@ import { SyncCrmService } from './sync-crm.service';
 import { SyncCrmController } from './sync-crm.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CouponRef } from '../coupon-ref/entities/coupon-ref.entity';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CouponRef])],
+  imports: [TypeOrmModule.forFeature([CouponRef, User])],
   controllers: [SyncCrmController],
   providers: [SyncCrmService],
 })
